701d0ad6d07f0007b2751c615369e1376068f71f
Sean Murphy <sean@iamseanmurphy.com>
2025-01-14 15:17:57 +0000

    1.4

--- preload-lcp-image/inc/options.php
+++ preload-lcp-image/inc/options.php
@@ -31,6 +31,14 @@ function preload_lcp_image_settings_init()
     );
 
     add_settings_field(
+        'preload_lcp_taxonomy_settings',
+        __('Posts Taxonomies to show', 'preload_lcp'),
+        'preload_lcp_taxonomy_settings_render',
+        'preload_LCP_Image',
+        'preload_lcp_admin_section'
+    );
+
+    add_settings_field(
         'preload_lcp_default_to_featured_image',
         __('Default to Featured Image', 'preload_lcp'),
         'preload_lcp_default_to_featured_image_render',
@@ -47,9 +55,7 @@ add_action('admin_init', 'preload_lcp_image_settings_init');
  * Blank because I haven't figured out another way to do this
  * @return void
  */
-function preload_lcp_display_settings_callback()
-{
-}
+function preload_lcp_display_settings_callback() {}
 
 
 /**
@@ -80,6 +86,38 @@ function preload_lcp_post_type_settings_render()
     }
     ?>
     <p class="description"><?php _e('Show the Preload LCP Image metabox on the chosen post type(s)', 'preload_lcp'); ?></p>
+    <?php
+}
+
+
+/**
+ * Render the post type settings option
+ * 
+ * @return void
+ */
+function preload_lcp_taxonomy_settings_render()
+{
+
+    $taxonomies       = get_taxonomies(array('public' => true), 'objects');
+
+    $taxonomies_to_show = preload_lcp_get_lcp_taxonomies();
+
+    if ($taxonomies) {
+
+        foreach ($taxonomies as $taxonomy) {
+
+            if (in_array($taxonomy->name, $taxonomies_to_show)) {
+                $checkedval = "checked";
+            } else {
+                $checkedval = "";
+            }
+    ?>
+            <input type='checkbox' name='preload_lcp_image_settings[preload_lcp_taxonomy_settings][]' value='<?php echo esc_attr($taxonomy->name); ?>' <?php echo esc_attr($checkedval); ?>><?php echo esc_attr($taxonomy->label); ?><br />
+    <?php
+        }
+    }
+    ?>
+    <p class="description"><?php _e('Show the Preload LCP Image metabox on the chosen taxonomies', 'preload_lcp'); ?></p>
 <?php
 }
 
@@ -92,8 +130,8 @@ function preload_lcp_post_type_settings_render()
 function preload_lcp_default_to_featured_image_render()
 {
 
-    $show_default = preload_lcp_get_option( 'preload_lcp_default_to_featured_image' );
-    
+    $show_default = preload_lcp_get_option('preload_lcp_default_to_featured_image');
+
 ?>
     <input type='checkbox' name='preload_lcp_image_settings[preload_lcp_default_to_featured_image]' value='show_featured_image' <?php checked($show_default, 'show_featured_image', true) ?>>
     <p class="description"><?php _e('Preload the featured image of the page or post as the LCP image if no other image is specified', 'preload-lcp-image'); ?></p>
@@ -135,11 +173,14 @@ function preload_lcp_image_options_page()
                     <p><img src="https://gravatar.com/avatar/13b432f781f24140731c6fe815e6d831?s=70&d=mm" alt="<?php _e('Rhys Wynne', 'preload_cp'); ?>" class="dr_avatar" />
                         <?php _e("Hello! Dwi'n Rhys (I am Rhys in Welsh), and I am an experienced WordPress developer from the United Kingdom, specialising in WordPress perfomance automation, as well as API integration, maintenance and custom code projects. Let's talk and see what I can do for you!", "preload_lcp"); ?>
                     </p>
-                    <p><a href="https://dwinrhys.com/wordpress-speed-optimisation/?utm_source=plugin-options&utm_medium=wordpress&utm_campaign=preload-lcp-image" target="_blank" class="dr_button dr_button_primary"><?php _e("Get your site optimised!", "preload_lcp"); ?></a>
+                    <p><a href="https://dwinrhys.com/wordpress-speed-optimisation/?utm_source=plugin-options&utm_medium=wordpress&utm_campaign=preload-lcp-image" target="_blank" class="dr_button dr_button_primary"><?php _e("Get your site optimised!", "preload_lcp"); ?></a></p>
+                </div>
+
+                <div class="dr_box">
+                    <?php dwinrhys_print_newsletter_box( 'preload_lcp' ); ?>
                 </div>
             </div>
         </div>
-
     </div>
 
 <?php
